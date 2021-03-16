a0bb76b1f7c7d4d9aa94f32d3d1d14e40600377c
jjuulliiaagg <Ty4kayes@yandex.ru>
2021-03-16 22:12:11 +0300

    change conftest

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 def browser():
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     browser = webdriver.Chrome('/usr/local/bin/chromedriver', options=chrome_options)
     browser.get('http://127.0.0.1:8000/')
