6d5b4073ab4f23550bfdc704d397c0596ff74208
Julia Gahovich <julia.gahovich@easybrain.com>
2021-03-16 11:36:55 +0300

    test

---
+++ pages/django_pages/base_pages.py
@@ -0,0 +1,8 @@
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.base_page = 'http://172.20.0.4:8000'
+
+    def open_base_page(self):
+        self.driver.get(self.base_page)
