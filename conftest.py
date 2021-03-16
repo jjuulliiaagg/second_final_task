919907e95fa3ed796687a51c663fec88584f7ffb
jjuulliiaagg <Ty4kayes@yandex.ru>
2021-03-16 13:02:56 +0300

    change conftest

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ def browser():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    browser = webdriver.Chrome(options=chrome_options)
+    browser = webdriver.Chrome('http://localhost:8000/', options=chrome_options)
     # browser.maximize_window()
     browser.implicitly_wait(5)
     yield browser
